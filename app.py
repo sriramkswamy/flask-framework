ec37e46cd913e329943378818df2b34766bb012c
Sriram Krishnaswamy <sriramkswamy@outlook.com>
2021-04-19 17:16:00 -0700

    Created standalone result.html file for the plots
    - Added another render_template for result.html that currently contains a simple bar chart
    - Created a result function that creates the simple bar chart mentioned above
    - Formatted the entire file with autopep8
    - The other defaults for this template is kept as is

--- app.py
+++ app.py
@@ -1,14 +1,52 @@
 from flask import Flask, render_template, request, redirect
 
+from bokeh.embed import components
+from bokeh.plotting import figure
+from bokeh.resources import INLINE
+from bokeh.util.string import encode_utf8
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
+
+
+@app.route('/result')
+def result():
+    # init a basic bar chart:
+    # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
+    fig = figure(plot_width=600, plot_height=600)
+    fig.vbar(
+        x=[1, 2, 3, 4],
+        width=0.5,
+        bottom=0,
+        top=[1.7, 2.2, 4.6, 3.9],
+        color='navy'
+    )
+
+    # grab the static resources
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
+    # render template
+    script, div = components(fig)
+    html = render_template(
+        'result.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources,
+    )
+
+    return render_template('result.html')
+
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507)
