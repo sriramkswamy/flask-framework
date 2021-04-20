59f3e61a7103a44a3e61a30e5dce670d79d9b50d
Sriram Krishnaswamy <sriramkswamy@outlook.com>
2021-04-19 17:20:05 -0700

    Removed UTF8 encoding that was causing an import error
    The error happened after deploying to Heroku

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, redirect
 from bokeh.embed import components
 from bokeh.plotting import figure
 from bokeh.resources import INLINE
-from bokeh.util.string import encode_utf8
 
 app = Flask(__name__)
 
